86af9b9f2914fc1f76500f9f8595f7806d445ad7
Artur <artur2p@gmail.com>
2021-10-04 19:38:07 +0200

    Adding auto-generated spring project

---
+++ src/test/java/io/artur/spring/sftpetclinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.sftpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
