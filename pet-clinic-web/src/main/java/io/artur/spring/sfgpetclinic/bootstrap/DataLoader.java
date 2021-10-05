97102ca2da7f95c7d12bbff1ec270155834f2de4
Artur Przychodzen <artur2p@gmail.com>
2021-10-05 18:27:32 +0200

    show list of vets

--- pet-clinic-web/src/main/java/io/artur/spring/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/io/artur/spring/sfgpetclinic/bootstrap/DataLoader.java
@@ -44,8 +44,8 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet2 = new Vet();
         vet2.setId(2L);
-        vet2.setFirstName("Sam");
-        vet2.setLastName("Axe");
+        vet2.setFirstName("Wendy");
+        vet2.setLastName("Knox");
         vetService.save(vet2);
         System.out.println("Loaded Vets.....");
     }
