9efd46bfd2bf488faf50bf1e4df053e37257d1ef
Artur Przychodzen <artur2p@gmail.com>
2021-10-06 16:35:11 +0200

    ptetypeservice implementation

---
+++ pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package io.artur.spring.sfgpetclinic.services;
+
+import io.artur.spring.sfgpetclinic.model.PetType;
+
+/**
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
