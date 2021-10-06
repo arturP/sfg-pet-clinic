46cf263b5e8bcc985eb2342cd1330e04c73c801a
Artur Przychodzen <artur2p@gmail.com>
2021-10-06 16:44:27 +0200

    PetTypes added to initialization process

--- pet-clinic-web/src/main/java/io/artur/spring/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/io/artur/spring/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package io.artur.spring.sfgpetclinic.bootstrap;
 
 import io.artur.spring.sfgpetclinic.model.Owner;
+import io.artur.spring.sfgpetclinic.model.PetType;
 import io.artur.spring.sfgpetclinic.model.Vet;
 import io.artur.spring.sfgpetclinic.services.OwnerService;
+import io.artur.spring.sfgpetclinic.services.PetTypeService;
 import io.artur.spring.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,14 +17,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+        System.out.println("Loaded PetTypes.....");
+
         Owner owner = new Owner();
         owner.setFirstName("Michael");
         owner.setLastName("Weston");
