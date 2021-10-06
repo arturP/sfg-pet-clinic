19adee0ae1daae996227e3141db7ed1299b7246e
Artur Przychodzen <artur2p@gmail.com>
2021-10-06 15:54:24 +0200

    specialty added

---
+++ pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package io.artur.spring.sfgpetclinic.model;
+
+/**
+ *
+ */
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/model/Vet.java
@@ -1,8 +1,18 @@
 package io.artur.spring.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  *
  */
 public class Vet extends Person{
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
