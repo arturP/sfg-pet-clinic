4969e2298aa99ce8d074c7228c9783646e82392a
Artur Przychodzen <artur2p@gmail.com>
2021-10-06 15:49:39 +0200

    visit entity added and owner updated

--- pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/io/artur/spring/sfgpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package io.artur.spring.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  *
  */
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
